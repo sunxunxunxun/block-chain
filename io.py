e60cd4a1da4ccdc7d5e1872bcc86cb7086397e7d
ssxun <34117516+sunxunxunxun@users.noreply.github.com>
2020-12-09 17:21:58 +0800

    Update io.py

--- io.py
+++ io.py
@@ -1,7 +1,7 @@
 import os
 
-RESULTS_PATH = "e_results/"
-RESULT_FILE = "e_results/io.txt"
+RESULTS_PATH = "p_results/"
+RESULT_FILE = "p_results/io.txt"
 
 log_files = os.listdir(RESULTS_PATH)
 res_file = open(RESULT_FILE, 'w')
@@ -14,31 +14,38 @@ for fl in log_files:
         lines = rf.readlines()
 
     totalWrite, totalRead = 0, 0
+    # {id: file}
     id2file = {}
     # {file: [write, read]}
     file2io = {}
-    for ll in lines:
-        ll = ll.strip()
-        if ll.find("total") == 0:
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip()
+        if lines[i].find("openat#") == 0:
             id2file = {}
-            continue
 
-        elif ll.find("coldplay coldplay 64") > 0:
-            idx, ff = ll.split(" ")[-3], ll.split(" ")[-1]
-            if ff.find("/home/coldplay/ewasm-node/data/geth/") == 0:
+        if lines[i].find("/home/coldplay/platon-node/data/") > 0:
+            if lines[i].find("openat#") == 0:
+                idx, ff = lines[i+1].strip().split("#")[-1], lines[i].split("#")[1]
                 id2file[idx] = ff
+                print(idx, ff)
                 if ff not in file2io:
                     file2io[ff] = [0, 0]
-                
 
-        elif ll.find("read#") == 0 or ll.find("write#") == 0:
-            length, idx = int(ll.split("#")[1]), ll.split("#")[2]
+            elif lines[i].find("coldplay coldplay 64") > 0:
+                idx, ff = lines[i].split(" ")[-3], lines[i].split(" ")[-1]
+                if ff.find("/home/coldplay/platon-node/data//") == 0:
+                    id2file[idx] = ff
+                    if ff not in file2io:
+                        file2io[ff] = [0, 0]
+                
+        elif lines[i].find("read#") == 0 or lines[i].find("write#") == 0:
+            length, idx = int(lines[i].split("#")[1]), lines[i].split("#")[2]
             if idx in id2file:
-                if ll.find("write#") == 0:
+                if lines[i].find("write#") == 0:
                     totalWrite += int(length)
                     file2io[id2file[idx]][0] += length
 
-                elif ll.find("read#") == 0:
+                elif lines[i].find("read#") == 0:
                     totalRead += int(length)
                     file2io[id2file[idx]][1] += length
     
