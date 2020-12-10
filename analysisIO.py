850159e357e0de17085763fe1984c17f55655ab7
sunxunxunxun <1553035744@qq.com>
2020-12-10 21:31:38 +0800

    update

--- analysisIO.py
+++ analysisIO.py
@@ -1,7 +1,7 @@
 import os
 
-RESULTS_PATH = "p_results/"
-RESULT_FILE = "p_results/io.txt"
+RESULTS_PATH = "../logs/o_results/"
+RESULT_FILE = "../results/o_io_all.txt"
 
 log_files = os.listdir(RESULTS_PATH)
 res_file = open(RESULT_FILE, 'w')
@@ -18,37 +18,37 @@ for fl in log_files:
     id2file = {}
     # {file: [write, read]}
     file2io = {}
+    file2io["unknown"] = [0, 0]
     for i in range(len(lines)):
         lines[i] = lines[i].strip()
         if lines[i].find("openat#") == 0:
             id2file = {}
+            idx, ff = lines[i+1].strip().split("#")[-1], lines[i].split("#")[1]
+            id2file[idx] = ff
+            print(idx, ff)
+            if ff not in file2io:
+                file2io[ff] = [0, 0]
 
-        if lines[i].find("/home/coldplay/platon-node/data/") > 0:
-            if lines[i].find("openat#") == 0:
-                idx, ff = lines[i+1].strip().split("#")[-1], lines[i].split("#")[1]
-                id2file[idx] = ff
-                print(idx, ff)
-                if ff not in file2io:
-                    file2io[ff] = [0, 0]
-
-            elif lines[i].find("coldplay coldplay 64") > 0:
-                idx, ff = lines[i].split(" ")[-3], lines[i].split(" ")[-1]
-                if ff.find("/home/coldplay/platon-node/data/") == 0:
-                    id2file[idx] = ff
-                    if ff not in file2io:
-                        file2io[ff] = [0, 0]
-                
+        elif lines[i].find("coldplay coldplay 64") > 0:
+            idx, ff = lines[i].split(" ")[-3], lines[i].split(" ")[-1]
+            if ff.find("socket") == 0:
+                ff = "socket"
+            elif ff.find("pipe") == 0:
+                ff = "pipe"
+            id2file[idx] = ff
+            if ff not in file2io:
+                file2io[ff] = [0, 0]
+            
         elif lines[i].find("read#") == 0 or lines[i].find("write#") == 0:
             length, idx = int(lines[i].split("#")[1]), lines[i].split("#")[2]
-            if idx in id2file:
-                if lines[i].find("write#") == 0:
-                    totalWrite += int(length)
-                    file2io[id2file[idx]][0] += length
+            ff = id2file.get(idx, "unknown")
+            if lines[i].find("write#") == 0:
+                file2io[ff][0] += length
+                totalWrite += int(length)
+            else:
+                file2io[ff][1] += length
+                totalRead += int(length)
 
-                elif lines[i].find("read#") == 0:
-                    totalRead += int(length)
-                    file2io[id2file[idx]][1] += length
-    
     print(totalWrite)
     print(totalRead)
     # print(file2io[f][0])
--- node.py
+++ node.py
@@ -1,21 +1,23 @@
 import os, re
 
-RESULTS_PATH = "o_results/"
-RESULT_FILE = "o_results/node.txt"
+RESULTS_PATH = "../logs/e_results/"
+RESULT_FILE = "../results/e_func_gas_executionTime.txt"
 
 log_files = os.listdir(RESULTS_PATH)
 res_file = open(RESULT_FILE, 'w')
-for fl in log_files:
-    if not (fl.find("node") == 0 and fl.find("log") > 0):
+for nodeFl in log_files:
+    if not (nodeFl.find("node") == 0 and nodeFl.find("log") > 0):
         continue
     lines = []
-    with open(RESULTS_PATH + fl, 'r', encoding='utf-8') as rf:
+    with open(RESULTS_PATH + nodeFl, 'r', encoding='utf-8') as rf:
         lines = rf.readlines()
-    print(fl)
+    print(nodeFl)
     deployGas, deployCnt = 0, 0
-    #{func: [totalGasUsed, times]}
-    gasUsed = {}
+    #{func: [totalGasUsed, totalTime, gasCount, exeCount]}
+    funcInfo = {}
+    function = []
 
+    # calculate gasUsed of function
     for i in range(len(lines)):
         if lines[i].find("contract deploy success, receipt:") == 0:
             while(1):
@@ -29,19 +31,51 @@ for fl in log_files:
         if lines[i].find("success") > 0 and lines[i].find("status: true") > 0:
             func = lines[i].split(" ")[0]
             gas = int(lines[i].split(" ")[-1].strip())
-            if func not in gasUsed:
-                gasUsed[func] = [0, 0]
-            gasUsed[func][0] += gas
-            gasUsed[func][1] += 1
+            if func not in funcInfo:
+                funcInfo[func] = [0, 0, 0, 0]
+                function.append(func)
+            funcInfo[func][0] += gas
+            funcInfo[func][2] += 1
     
+    print(function)
+    # calculate execution time of func
+    if len(function):
+        runFl = "run_" + "_".join(nodeFl.split('_')[1:])
+        print(runFl)
+        lines = []
+        with open(RESULTS_PATH + runFl, 'r', encoding='utf-8') as rf:
+            lines = rf.readlines()
+        
+        start, end = 0, 0
+        funcId = 0
+        cnt = 0
+        for line in lines:
+            # for openethereum
+            if line.find("#execute#start#") > 0:
+            # for platon
+            # if line.find("#start#") > 0:
+                if cnt == 0:
+                    cnt += 1
+                    continue
+                start = int(line.split('#')[0])
+                cnt = (cnt + 1) % 101
+            #for openethereum
+            if (line.find("#execute#end#") > 0 or line.find("#execute#End#") > 0) and start:
+            # for platon
+            # if (line.find("#end#") > 0 or line.find("#End#") > 0) and start:
+                end = int(line.split('#')[0])
+                func = function[funcId]
+                funcInfo[func][1] += end - start
+                funcId = (funcId + 1) % len(function)
+                funcInfo[func][3] += 1
+                start = 0
+
     if deployCnt:
-        res_file.write("%s\ndeployGas: %d\n" % (fl, deployGas / deployCnt))
-    for func in gasUsed:
-        if gasUsed[func][1]:
-            res_file.write("function: %s gasUsed: %d\n" % (func, gasUsed[func][0] / gasUsed[func][1]))
-    if deployCnt:
+        res_file.write("%s\ndeployGas: %d\n" % ("_".join(nodeFl.split('_')[1:]), deployGas / deployCnt))
+    for func in funcInfo:
+        if funcInfo[func][2]:
+            res_file.write("function: %s#gasUsed: %d" % (func, funcInfo[func][0] / funcInfo[func][2]))
+        if funcInfo[func][3]:
+            res_file.write("#execution time: %d" % (funcInfo[func][1] / funcInfo[func][3]))
         res_file.write("\n")
-
-
-
-    
+    res_file.write("\n")
